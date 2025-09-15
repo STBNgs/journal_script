8d1f86b8f73a5e86fbdfb75c9529c22fc8ab1826
STBNgs <estebandido51@gmail.com>
2025-09-15 12:53:24 -0300

    updated main script

--- journal.py
+++ journal.py
@@ -3,3 +3,7 @@ import datetime
 
 script_path = os.getcwd()
 entries_path = os.path.join(script_path, "entries") 
+
+if not entries_path:
+    os.mkdir(entries_path)
+    print("entries folder has been created")
