45f3b024cc463336d4ac964ab491dec6deabdf87
STBNgs <estebandido51@gmail.com>
2025-09-15 15:20:30 -0300

    fixed entries folder creation

--- journal.py
+++ journal.py
@@ -4,6 +4,6 @@ import datetime
 script_path = os.getcwd()
 entries_path = os.path.join(script_path, "entries") 
 
-if not entries_path:
+if not os.path.exists(entries_path):
     os.mkdir(entries_path)
     print("entries folder has been created")
